64a7c3a6751ed079bc0d9857bd69d5052e42d557
pauloagudelo <pagudeloj@gmail.com>
2016-10-11 09:45:39 -0500

    Add files via upload

---
+++ Lina.mod.ino
@@ -0,0 +1,44 @@
+
+const int sensorPin= A0;
+const int electro = 10;
+int humedad;//Valor de la humedad actual de la planta
+int humedadcorte;//Humedad que fija el usuario girando el potenciómetro.
+int control=8;//Pin que activa o no el relé
+
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  pinMode(electro, OUTPUT);
+  //lcd.begin(16, 2);
+  Serial.begin(9600);
+  // Print a message to the LCD.
+ //
+ humedad=0.12*analogRead(0);//Convierte la lectura del sensor a %.
+ humedadcorte=0.07*analogRead(1)+15;//Convierte la lectura del potenciómetro a %. La
+
+}
+
+void loop() {
+  
+ int value = analogRead(sensorPin);
+  float millivolts = (value / 1023.0) * 5000;
+  float celsius = millivolts / 10; 
+  //lcd.print(celsius);>
+  
+ 
+   int humedad = analogRead(sensorPin);
+   // lcd.print(humedad);
+   digitalWrite(electro, HIGH); 
+    if(humedad > 500) {
+     digitalWrite(electro, HIGH);
+    // lcd.print("Apagado");
+    }  
+   else
+   {
+        digitalWrite(electro, LOW);
+     
+   }
+  delay(1000);
+
+}
+
---
+++ bluetoth.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h>
+#define TxD 0
+#define RxD 1
+#define KEY 4
+#define VCC 5
+
+int valor; 
+
+SoftwareSerial Bluetooth(TxD,RxD);
+
+void setup() { 
+  Bluetooth.begin(9600);
+  pinMode(KEY,OUTPUT);
+  pinMode(VCC,OUTPUT);
+
+  digitalWrite(KEY,HIGH);
+  digitalWrite(VCC,LOW);
+
+  digitalWrite(VCC,HIGH);
+
+  Serial.begin (9600);
+   
+}
+ void loop(){
+  valor= analogRead(A0);
+  Serial.println(valor);
+  delay(1000);
+  char comando = Bluetooth .read();
+  switch(comando){
+    Bluetooth.println(valor);
+    delay(1000);
+    break;
+    
+    
+  }
+ }
+
